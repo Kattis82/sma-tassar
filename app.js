6270d639d223a0b612cdb9fe923eb8a60efb92ff
Kattis82 <kattiscalmvik@outlook.com>
2026-08-24 13:25:04 +0200

    Add interactive shopping cart panel

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Små Tassar JavaScript är igång!");
 
-const buyButtons = document.querySelectorAll(".buyButton");
+const buyButtons = document.querySelectorAll(".buy-button");
 console.log(buyButtons);
 
 buyButtons.forEach((button) => {
@@ -9,4 +9,23 @@ buyButtons.forEach((button) => {
 
     alert(`Vara lagd i varukorg: ${productName}`);
   });
+});
+
+// hämtar varukorgen och knapparna för att dölja/visa den
+const cart = document.querySelector("#cart");
+const cartButton = document.querySelector("#cart-button");
+const closeCartButton = document.querySelector("#close-cart");
+
+// när man klickar på menylänken visas varukorgen
+cartButton.addEventListener("click", (event) => {
+  event.preventDefault(); // gör inte länkens vanliga beteende
+  cart.classList.remove("hidden");
+
+  // scrolla ned till varukorgen
+  cart.scrollIntoView({ behavior: "smooth" });
+});
+
+// när man klickar på stäng så döljs varukorgen
+closeCartButton.addEventListener("click", () => {
+  cart.classList.add("hidden");
 });
