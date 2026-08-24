73ea7bcb3cb9a78a01027ba1f95f4daf87dbc11f
Kattis82 <kattiscalmvik@outlook.com>
2026-08-24 19:51:33 +0200

    Add interactive shopping cart with quantity selection

--- app.js
+++ app.js
@@ -3,11 +3,46 @@ console.log("Små Tassar JavaScript är igång!");
 const buyButtons = document.querySelectorAll(".buy-button");
 console.log(buyButtons);
 
+// objektet (varukorgen) som ska innehålla produkter (nyckel) och antal (värde)
+const cartItems = {};
+
+// funktion som visar innehållet i varukorgen
+function updateCart() {
+  const cartList = document.querySelector("#cart-items");
+
+  // töm hela listan innan den fylls igen, för att inte samma produkter
+  // ska skrivas ut om och om igen
+  cartList.innerHTML = "";
+
+  // gå igenom alla produkter i varukorgen
+  for (const productName in cartItems) {
+    // skapar ett nytt HTML-element
+    const listItem = document.createElement("li");
+    listItem.textContent = `${productName} (${cartItems[productName]} st)`;
+
+    // lägger in <li> i <ul>
+    cartList.appendChild(listItem);
+  }
+}
+
 buyButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const productName = button.dataset.name;
 
-    alert(`Vara lagd i varukorg: ${productName}`);
+    // hämtar antal-fältet som hör till den här produkten
+    const quantityInput = button.previousElementSibling; // vilket HTML-element ligger precis före button
+
+    // hämtar värdet från antal-fältet och gör om det till ett nummer
+    const quantity = Number(quantityInput.value);
+
+    // öka antalet med det valt antal
+    if (cartItems[productName]) {
+      cartItems[productName] += quantity;
+    } else {
+      cartItems[productName] = quantity;
+    }
+
+    updateCart();
   });
 });
 
